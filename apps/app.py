2c8c96cd8bfc5a032af0e935afbe49a35df6bf17
Eric Ma <ericmajinglong@gmail.com>
2023-02-26 14:41:56 -0500

    Add panel to environment definition.

---
+++ apps/app.py
@@ -0,0 +1,28 @@
+"""Test panel app."""
+import panel as pn
+from dotenv import load_dotenv
+from pyprojroot import here
+
+load_dotenv()
+
+pn.extension()
+
+
+def user_is_authorized(user_info: dict):
+    """Check if the user is authorized to access the app.
+
+
+    :param user_info: The user information.
+    :returns: True if the user is authorized, False otherwise.
+    """
+    with open(here() / "apps/users.txt") as f:
+        valid_users = [line.strip("\n") for line in f.readlines()]
+
+    print(user_info["login"] in valid_users)
+    return user_info["login"] in valid_users
+
+
+pn.config.authorize_callback = user_is_authorized
+
+
+pn.Column(pn.pane.Markdown("# Header!")).servable()
